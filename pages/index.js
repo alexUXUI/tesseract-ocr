089ef21e724fbc33572824db5b68cee8aaf3293b
Alex Bennett <alexandergraybennett@gmail.com>
2022-09-30 15:37:34 -0600

    fix import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-// import Tesseract from 'tesseract.js';
+import Tesseract from 'tesseract.js';
 import styles from '../styles/Home.module.css';
 
 import { createWorker } from 'tesseract.js';
